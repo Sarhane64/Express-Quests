b428674d3686d5dc06caa630efdf005e0236a119
jean519 <129189905+jean519@users.noreply.github.com>
2023-11-06 11:14:32 +0100

    ajout de post

--- src/app.js
+++ src/app.js
@@ -4,11 +4,15 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const movieControllers = require("./controllers/movieControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", movieControllers.getUser);
 app.get("/api/users/:id", movieControllers.getUserById);
+app.post("/api/movies", movieControllers.postMovie);
+app.post("/api/users", movieControllers.postUser);
 
 module.exports = app;
--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -74,6 +74,40 @@ const getMovieById = (req, res) => {
     });
 };
 
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.status(201).send({ id: result.insertId });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const postUser = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.status(201).send({ id: result.insertId });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
   database.query("select * from users where id = ?", [id]).then(([users]) => {
@@ -90,4 +124,6 @@ module.exports = {
   getMovieById,
   getUser,
   getUserById,
+  postUser,
+  postMovie,
 };
