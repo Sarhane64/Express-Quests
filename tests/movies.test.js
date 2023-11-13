459e6552fa4c626d082cd61cd62b98579b945fe1
jean519 <129189905+jean519@users.noreply.github.com>
2023-11-13 12:07:46 +0100

    ajout des test de la route poste

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-
+const crypto = require("node:crypto");
 const app = require("../src/app");
 
 describe("GET /api/movies", () => {
@@ -25,5 +25,56 @@ describe("GET /api/movies/:id", () => {
     const response = await request(app).get("/api/movies/0");
 
     expect(response.status).toEqual(404);
+  });
+});
+describe("POST /api/movies", () => {
+  it("should return created movie", async () => {
+    const newMovie = {
+      title: "Star Wars",
+      director: "George Lucas",
+      year: "1977",
+      color: true,
+      duration: 120,
+    };
+
+    const response = await request(app).post("/api/movies").send(newMovie);
+
+    expect(response.status).toEqual(201);
+    expect(response.body).toHaveProperty("id");
+    expect(typeof response.body.id).toBe("number");
+
+    const getResponse = await request(app).get(
+      `/api/movies/${response.body.id}`
+    );
+
+    expect(getResponse.headers["content-type"]).toMatch(/json/);
+    expect(getResponse.status).toEqual(200);
+
+    expect(getResponse.body).toHaveProperty("id");
+
+    expect(getResponse.body).toHaveProperty("title");
+    expect(getResponse.body.title).toStrictEqual(newMovie.title);
+
+    expect(getResponse.body).toHaveProperty("director");
+    expect(getResponse.body.director).toStrictEqual(newMovie.director);
+
+    expect(getResponse.body).toHaveProperty("year");
+    expect(getResponse.body.year).toStrictEqual(newMovie.year);
+
+    expect(getResponse.body).toHaveProperty("color");
+    expect(getResponse.body.color).toStrictEqual("1");
+
+    expect(getResponse.body).toHaveProperty("title");
+    expect(getResponse.body.duration).toStrictEqual(newMovie.duration);
+  });
+
+  it("should return an error", async () => {
+    const movieWithMissingProps = { title: "Harry Potter" };
+
+    const response = await request(app)
+      .post("/api/movies")
+      .send(movieWithMissingProps);
+
+    expect(response.status).toEqual(500);
   });
 });
